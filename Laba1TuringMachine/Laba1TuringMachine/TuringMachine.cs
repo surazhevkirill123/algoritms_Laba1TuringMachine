5691312512f04c7bac994c6ca7446dd1963c308b
Kirill Surazhev <surazhevkirillleonidovich@gmail.com>
2021-03-05 12:29:57 +0300

    Add fixes

--- Laba1TuringMachine/Laba1TuringMachine/TuringMachine.cs
+++ Laba1TuringMachine/Laba1TuringMachine/TuringMachine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Laba1TuringMachine
@@ -7,21 +8,15 @@ namespace Laba1TuringMachine
     {
         public static char[,,] statusTable;//первое измерение - реакции на алфавит согласно его порядку, второе - состояния, третье - набор действий(напр. _>2-заменить символ на _,сдвинуть вправо,перейти во второе состояние)
         public static char[] lenta;
-        public static char[] alphabet;
-        public static void TuringMachineAlgorithm(char[] alphabet, char[] lenta, int lentaIndexNow, char[,,] statusTable)
+        public static Dictionary<char,int> alphabet;
+        public static void TuringMachineAlgorithm(Dictionary<char, int> alphabet, char[] lenta, int lentaIndexNow, char[,,] statusTable)
         {
             int statusNow = 1;
             int alphabetLetterNow = 0;
+            
             while (statusNow != 0)
             {
-                for (int i = 0; i < alphabet.Length; i++)
-                {
-                    if (lenta[lentaIndexNow].Equals(alphabet[i]))
-                    {
-                        alphabetLetterNow = i;
-                    }
-                }
-
+                alphabetLetterNow = alphabet[lenta[lentaIndexNow]];
                 lenta[lentaIndexNow] = statusTable[alphabetLetterNow, statusNow - 1, 0];
                 if (statusTable[alphabetLetterNow, statusNow - 1, 1].Equals('>'))
                 {
@@ -37,7 +32,13 @@ namespace Laba1TuringMachine
 
         static void Main(string[] args)
         {
-            char[] alphabet = { 'a', 'b', '_' };
+            
+            Dictionary<char, int> alphabet = new Dictionary<char, int>
+            {
+                ['a'] = 0,
+                ['b'] = 1,
+                ['_'] = 2
+            };
             char[] lenta = { '_', '_', '_', '_', 'a', 'a', 'a', 'b', '_', '_', '_', '_' };
             int lentaIndexNow = 4; //мы договариваемся, что с самого начала головка стоит на первой позиции нужного нам слова
             char[,,] statusTable = {
@@ -45,10 +46,17 @@ namespace Laba1TuringMachine
                 { { '_','>','3' } , { 'b','>','2' } , { 'b','>','3' } },
                 { { 'n','n','n' } , { 'a','.','0' } , { 'b','.','0' } }
             };
+            Console.WriteLine(alphabet['a']);
             TuringMachineAlgorithm(alphabet, lenta, lentaIndexNow, statusTable);
             Console.WriteLine(lenta);
 
-            char[] alphabet1 = { 'a', 'b', '*' ,'_' };
+            Dictionary<char, int> alphabet1 = new Dictionary<char, int>
+            {
+                ['a'] = 0,
+                ['b'] = 1,
+                ['*'] = 2,
+                ['_'] = 3
+            };
             char[] lenta1 = { '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', 'a', 'b', 'a', 'b', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' };
             int lentaIndexNow1 = 12;
             char[,,] statusTable1 = {
